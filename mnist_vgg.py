4c83afb7695e2062098acb499b77a751e8fbbe19
eunah <emainema@gmail.com>
2017-07-14 13:40:32 +0200

    pretrained model (vgg16) test on mnist

---
+++ mnist_vgg.py
@@ -0,0 +1,288 @@
+import numpy as np
+import io
+import urllib
+import os
+import sys
+import time
+import cPickle as pickle
+
+import theano
+import theano.tensor as T
+import lasagne
+from lasagne.layers import InputLayer, DenseLayer, DropoutLayer, NonlinearityLayer
+from lasagne.layers import Conv2DLayer as ConvLayer
+from lasagne.layers import MaxPool2DLayer as PoolLayer
+
+
+def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
+    assert len(inputs) == len(targets)
+    if shuffle:
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+    for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batchsize]
+        else:
+            excerpt = slice(start_idx, start_idx + batchsize)
+        yield inputs[excerpt], targets[excerpt]
+
+# Download and prepare the MNIST dataset
+def load_dataset():
+    # We first define a download function, supporting both Python 2 and 3.
+    if sys.version_info[0] == 2:
+        from urllib import urlretrieve
+    else:
+        from urllib.request import urlretrieve
+
+    def download(filename, source='http://yann.lecun.com/exdb/mnist/'):
+        print("Downloading %s" % filename)
+        urlretrieve(source + filename, filename)
+
+    # We then define functions for loading MNIST images and labels.
+    # For convenience, they also download the requested files if needed.
+    import gzip
+
+    def load_mnist_images(filename):
+        if not os.path.exists(filename):
+            download(filename)
+        # Read the inputs in Yann LeCun's binary format.
+        with gzip.open(filename, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=16)
+        # The inputs are vectors now, we reshape them to monochrome 2D images,
+        # following the shape convention: (examples, channels, rows, columns)
+        data = data.reshape(-1, 1, 28, 28)
+
+        # To make 3 channels data
+        rgb_data = np.zeros([data.shape[0], 3, 28, 28])
+        for i in range(data.shape[0]) :
+            rgb_data[i][0] = data[i][0]
+            rgb_data[i][1] = data[i][0]
+            rgb_data[i][2] = data[i][0]
+
+        # The inputs come as bytes, we convert them to float32 in range [0,1].
+        # (Actually to range [0, 255/256], for compatibility to the version
+        # provided at http://deeplearning.net/data/mnist/mnist.pkl.gz.)
+        return rgb_data / np.float32(256)
+
+    def load_mnist_labels(filename):
+        if not os.path.exists(filename):
+            download(filename)
+        # Read the labels in Yann LeCun's binary format.
+        with gzip.open(filename, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=8)
+        # The labels are vectors of integers now, that's exactly what we want.
+        return data
+
+    # We can now download and read the training and test set images and labels.
+    X_train = load_mnist_images('train-images-idx3-ubyte.gz')
+    y_train = load_mnist_labels('train-labels-idx1-ubyte.gz')
+    X_test = load_mnist_images('t10k-images-idx3-ubyte.gz')
+    y_test = load_mnist_labels('t10k-labels-idx1-ubyte.gz')
+
+    # We reserve the last 10000 training examples for validation.
+    X_train, X_val = X_train[:-10000], X_train[-10000:]
+    y_train, y_val = y_train[:-10000], y_train[-10000:]
+
+    # We just return all the arrays in order, as expected in main().
+    # (It doesn't matter how we do this as long as we can read them again.)
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+# Build model VGG16 architecture
+def build_model_vgg16():
+    net = {}
+    net['input'] = InputLayer((None, 3, 224, 224))
+    net['conv1_1'] = ConvLayer(net['input'], 64, 3, pad=1, flip_filters=False)
+    net['conv1_2'] = ConvLayer(net['conv1_1'], 64, 3, pad=1, flip_filters=False)
+    net['pool1'] = PoolLayer(net['conv1_2'], 2)
+    net['conv2_1'] = ConvLayer(net['pool1'], 128, 3, pad=1, flip_filters=False)
+    net['conv2_2'] = ConvLayer(net['conv2_1'], 128, 3, pad=1, flip_filters=False)
+    net['pool2'] = PoolLayer(net['conv2_2'], 2)
+    net['conv3_1'] = ConvLayer(net['pool2'], 256, 3, pad=1, flip_filters=False)
+    net['conv3_2'] = ConvLayer(net['conv3_1'], 256, 3, pad=1,   flip_filters=False)
+    net['conv3_3'] = ConvLayer(net['conv3_2'], 256, 3, pad=1, flip_filters=False)
+    net['pool3'] = PoolLayer(net['conv3_3'], 2)
+    net['conv4_1'] = ConvLayer(net['pool3'], 512, 3, pad=1, flip_filters=False)
+    net['conv4_2'] = ConvLayer(net['conv4_1'], 512, 3, pad=1, flip_filters=False)
+    net['conv4_3'] = ConvLayer(net['conv4_2'], 512, 3, pad=1, flip_filters=False)
+    net['pool4'] = PoolLayer(net['conv4_3'], 2)
+    net['conv5_1'] = ConvLayer(net['pool4'], 512, 3, pad=1, flip_filters=False)
+    net['conv5_2'] = ConvLayer(net['conv5_1'], 512, 3, pad=1, flip_filters=False)
+    net['conv5_3'] = ConvLayer(net['conv5_2'], 512, 3, pad=1, flip_filters=False)
+    net['pool5'] = PoolLayer(net['conv5_3'], 2)
+    net['fc6'] = DenseLayer(net['pool5'], num_units=4096)
+    net['fc6_dropout'] = DropoutLayer(net['fc6'], p=0.5)
+    net['fc7'] = DenseLayer(net['fc6_dropout'], num_units=4096)
+    net['fc7_dropout'] = DropoutLayer(net['fc7'], p=0.5)
+    net['fc8'] = DenseLayer(net['fc7_dropout'], num_units=1000, nonlinearity=None)
+    net['prob'] = NonlinearityLayer(net['fc8'], lasagne.nonlinearities.softmax)
+    net['output_layer'] = net['prob']
+
+    return net
+
+# FC network from mnist example of lasagne
+def build_mlp(input_var=None):
+    # Input : MNIST image in 3 channels
+    l_in = lasagne.layers.InputLayer(shape=(None, 3, 28, 28),
+                                     input_var=input_var)
+
+    # droupout of 20%
+    l_in_drop = lasagne.layers.DropoutLayer(l_in, p=0.2)
+
+    l_hid1 = lasagne.layers.DenseLayer(
+            l_in_drop, num_units=800,
+            nonlinearity=lasagne.nonlinearities.rectify,
+            W=lasagne.init.GlorotUniform())
+
+    # dropout of 50%:
+    l_hid1_drop = lasagne.layers.DropoutLayer(l_hid1, p=0.5)
+
+    l_hid2 = lasagne.layers.DenseLayer(
+            l_hid1_drop, num_units=800,
+            nonlinearity=lasagne.nonlinearities.rectify)
+
+    # 50% dropout again:
+    l_hid2_drop = lasagne.layers.DropoutLayer(l_hid2, p=0.5)
+
+    l_out = lasagne.layers.DenseLayer(
+            l_hid2_drop, num_units=10,
+            nonlinearity=lasagne.nonlinearities.softmax)
+
+    return l_out
+
+# Build network for MNIST with pretrained network VGG
+def build_cnn(input_var, pretrained_model):
+
+    conv1_1 = pretrained_model['conv1_1']
+    conv1_2 = pretrained_model['conv1_2']
+
+    network = lasagne.layers.InputLayer(shape=(None, 3, 28, 28),
+                                        input_var=input_var)
+    network = lasagne.layers.Conv2DLayer(
+            network, 64, 3, pad=1, flip_filters=False, W=conv1_1.W.get_value(), b=conv1_1.b.get_value())
+    network = lasagne.layers.Conv2DLayer(
+            network, 64, 3, pad=1, flip_filters=False, W=conv1_2.W.get_value(), b=conv1_2.b.get_value())
+    network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
+
+    network = lasagne.layers.DenseLayer(
+            lasagne.layers.dropout(network, p=.5),
+            num_units=256,
+            nonlinearity=lasagne.nonlinearities.rectify)
+
+    network = lasagne.layers.DenseLayer(
+            lasagne.layers.dropout(network, p=.5),
+            num_units=10,
+            nonlinearity=lasagne.nonlinearities.softmax)
+
+    return network
+
+
+####### Main #######
+
+####### Load pretrained model #######
+pretrained_model = 'vgg16.pkl'
+print 'load pretrained model: ', pretrained_model
+model = pickle.load(open('models/'+pretrained_model, 'rb'))
+MEAN_IMAGE = np.array([np.full((224, 224), model['mean value'][0]), np.full((224, 224), model['mean value'][1]), np.full((224, 224), model['mean value'][2])])
+CLASSES = model['synset words']
+
+# Set pretrained model values
+print 'set pretrained model values'
+pretrained_model = build_model_vgg16()
+lasagne.layers.set_all_param_values(pretrained_model['output_layer'], model['param values'])
+
+####### Load the dataset #######
+print("Loading data...")
+X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
+
+# Prepare Theano variables for inputs and targets
+input_var = T.tensor4('inputs')
+target_var = T.ivector('targets')
+
+####### Create neural network model #######
+print("Building model and compiling functions...")
+# network = build_mlp(input_var) # Simple FC network to check basic MNIST network
+network = build_cnn(input_var, pretrained_model) # Build cnn model with pretrained model
+
+####### Training Phase #######
+# Training params
+num_epochs = 10
+
+# Create a loss expression for training, i.e., a scalar objective we want
+# to minimize (for our multi-class problem, it is the cross-entropy loss):
+prediction = lasagne.layers.get_output(network)
+loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+loss = loss.mean()
+# We could add some weight decay as well here, see lasagne.regularization.
+
+# Create update expressions for training, i.e., how to modify the
+# parameters at each training step. Here, we'll use Stochastic Gradient
+# Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
+params = lasagne.layers.get_all_params(network, trainable=True)
+updates = lasagne.updates.nesterov_momentum(
+    loss, params, learning_rate=0.01, momentum=0.9)
+
+# Create a loss expression for validation/testing. The crucial difference
+# here is that we do a deterministic forward pass through the network,
+# disabling dropout layers.
+test_prediction = lasagne.layers.get_output(network, deterministic=True)
+test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
+                                                        target_var)
+test_loss = test_loss.mean()
+# As a bonus, also create an expression for the classification accuracy:
+test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
+                  dtype=theano.config.floatX)
+
+# Compile a function performing a training step on a mini-batch (by giving
+# the updates dictionary) and returning the corresponding training loss:
+train_fn = theano.function([input_var, target_var], loss, updates=updates)
+
+# Compile a second function computing the validation loss and accuracy:
+val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
+
+# Finally, launch the training loop.
+print("Starting training...")
+# We iterate over epochs:
+for epoch in range(num_epochs):
+    # In each epoch, we do a full pass over the training data:
+    train_err = 0
+    train_batches = 0
+    start_time = time.time()
+    for batch in iterate_minibatches(X_train, y_train, 500, shuffle=True):
+        inputs, targets = batch
+        train_err += train_fn(inputs, targets)
+        train_batches += 1
+
+    # And a full pass over the validation data:
+    val_err = 0
+    val_acc = 0
+    val_batches = 0
+    for batch in iterate_minibatches(X_val, y_val, 500, shuffle=False):
+        inputs, targets = batch
+        err, acc = val_fn(inputs, targets)
+        val_err += err
+        val_acc += acc
+        val_batches += 1
+
+    # Then we print the results for this epoch:
+    print("Epoch {} of {} took {:.3f}s".format(
+        epoch + 1, num_epochs, time.time() - start_time))
+    print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
+    print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
+    print("  validation accuracy:\t\t{:.2f} %".format(
+        val_acc / val_batches * 100))
+
+# After training, we compute and print the test error:
+test_err = 0
+test_acc = 0
+test_batches = 0
+for batch in iterate_minibatches(X_test, y_test, 500, shuffle=False):
+    inputs, targets = batch
+    err, acc = val_fn(inputs, targets)
+    test_err += err
+    test_acc += acc
+    test_batches += 1
+print("Final results:")
+print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
+print("  test accuracy:\t\t{:.2f} %".format(
+    test_acc / test_batches * 100))
+
