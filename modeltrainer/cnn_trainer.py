42e004eb92f5c7934faa9a7c1e1f1e0a3f2259ec
Maximilian Schukraft <m.schukraft@gmx.de>
2017-07-24 22:40:19 +0200

    save accuracy and loss functions as picture, save them and param.pkl in a experiment folder

--- modeltrainer/cnn_trainer.py
+++ modeltrainer/cnn_trainer.py
@@ -7,23 +7,58 @@ import time
 from utils.data_iterator import iterate_minibatches
 import numpy as np
 from models.cnn_models import build_cnn
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 import cPickle as pickle
+import datetime
+import os
 
 from models.cnn_models import build_shallow_cnn
 from models.pretrained_models import build_vgg_cnn
 
 theano.config.floatX = 'float32'
 
-def train_model(networkname = None, num_epochs = 10, batch_size = 200):
+def train_model(networkname = None, num_epochs = 10, batch_size = 200, save_params = False, save_step=5):
+#Please write some information about your current experiment into the "additionalInfo" Variable, so you know later which folder belongs to which experiment
+
+    SAVE_BEST_PARAMS = save_params #if true, params are saved as pickle file every SAVE_STEP step
+    SAVE_STEP = save_step
+
     weight_decay =1e-6
     losses = {}
     training_loss_history = []
     validation_loss_history = []
+    test_loss_history = []
+    val_acc_history = []
+    test_acc_history = []
     input_var = T.tensor4('inputs', dtype=theano.config.floatX)
     target_var = T.ivector('targets')
 
-    print('starting to create network...')
+    # create new experiment folder
+    dateTimeOfExperiment = str(datetime.datetime.today().strftime('%Y_%m_%d_%H_%M_%S'))
+    additionalInfo = '_first2PretrainedConv'  # add additional Info about the experiment you are runneing
+    experimentname = dateTimeOfExperiment + additionalInfo
+    experimentpath = 'experiments/' + experimentname + '/'
+    if not os.path.exists(experimentpath):
+        os.makedirs(experimentpath)
 
+    if SAVE_BEST_PARAMS ==True:
+        paramsPath = experimentpath + 'bestParams/'
+        if not os.path.exists(paramsPath):
+            os.makedirs(paramsPath)
+
+    # accuracyPath = experimentpath + 'accuracy/'
+    # if not os.path.exists(accuracyPath):
+    #     os.makedirs(accuracyPath)
+    # lossPath = experimentpath + 'loss/'
+    # if not os.path.exists(lossPath):
+    #     os.makedirs(lossPath)
+
+
+
+
+    print('starting to create network...')
     if networkname.startswith('cnn'):
         print('load fer data')
         train_data = load_fer(0, one_hot=False, flat=False)
@@ -109,32 +144,67 @@ def train_model(networkname = None, num_epochs = 10, batch_size = 200):
             epoch + 1, num_epochs, time.time() - start_time))
         print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
         print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
-        print("  validation accuracy:\t\t{:.2f} %".format(
-            val_acc / val_batches * 100))
+        print("  validation accuracy:\t\t{:.2f} %".format(val_acc / val_batches * 100))
         training_loss_history.append(train_err/train_batches)
         validation_loss_history.append(val_err / val_batches)
 
-        if (epoch % 5) == 0:
-            print ('saving network')
-            params = lasagne.layers.get_all_param_values(network)
-            paramname = 'shallow_cnn_trained' + str(epoch) + '.pkl'
-            pickle.dump(params, open(paramname, 'wb'))
-            print('network saved')
+        # After training of each epoch, we compute and print the test error:
+        test_err = 0
+        test_acc = 0
+        test_batches = 0
+        for batch in iterate_minibatches(test_data['data'], test_data['target'], 500, shuffle=False):
+            inputs, targets = batch
+            err, acc = val_fn(inputs, targets)
+            test_err += err
+            test_acc += acc
+            test_batches += 1
+        print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
+        print("  test accuracy:\t\t{:.2f} %".format(
+            test_acc / test_batches * 100))
+        test_loss_history.append(test_err / test_batches)
+        test_acc_history.append(test_acc / test_batches)
+        val_acc_history.append(val_acc / val_batches)
 
-    # After training, we compute and print the test error:
-    test_err = 0
-    test_acc = 0
-    test_batches = 0
-    for batch in iterate_minibatches(test_data['data'], test_data['target'], 500, shuffle=False):
-        inputs, targets = batch
-        err, acc = val_fn(inputs, targets)
-        test_err += err
-        test_acc += acc
-        test_batches += 1
-    print("Final results:")
-    print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
-    print("  test accuracy:\t\t{:.2f} %".format(
-        test_acc / test_batches * 100))
+        if SAVE_BEST_PARAMS == True:
+            if (epoch % SAVE_STEP) == 0:
+                print ('saving network...')
+                params = lasagne.layers.get_all_param_values(network)
+                paramname = paramsPath + 'best_params_epoch_' + str(epoch).zfill(2) + '.pkl'
+                pickle.dump(params, open(paramname, 'wb'))
+                #print('network saved')
+
+        # Plot the loss functions after every epoch
+        plt.gca().cla()
+        plt.plot(training_loss_history, label="train loss")
+        plt.plot(validation_loss_history, label="validation loss")
+        plt.plot(test_loss_history, label="test loss")
+        plt.legend()
+        plt.xlabel("Epoch")
+        plt.ylabel("Cost")
+        plt.title('Loss until Epoch  ' + str(epoch) + ' Validation Accuracy: ' + str(val_acc / val_batches * 100))
+        plt.show()
+        plt.savefig(experimentpath + 'Loss')
+        #plt.savefig(lossPath + 'Loss_Epoch_' + str(epoch).zfill(2))
+
+
+        # Save the losses of every epoch of training, validation and test so we can resume
+        pickle.dump((training_loss_history, validation_loss_history, test_loss_history), open(experimentpath + 'loss_history.pkl', 'wb'))
+
+        # Plot the test accuracy after each epoch
+        plt.gca().cla()
+        plt.plot(test_acc_history, label="test acc")
+        plt.plot(val_acc_history, label="validation acc")
+        plt.legend()
+        plt.xlabel("Epoch")
+        plt.ylabel("Accuracy")
+        plt.title('Accuracy until Epoch  ' + str(epoch) + ' Test Acc: ' + str(test_acc/test_batches *100))
+        plt.show()
+        plt.savefig(experimentpath + 'Accuracy')
+        #plt.savefig(accuracyPath + 'Acc_Epoch_' + str(epoch).zfill(2))
+
+
+
+
 
     #np.savez('model.npz', *lasagne.layers.get_all_param_values(network))
     # Optionally, you could now dump the network weights to a file like this:
@@ -144,6 +214,7 @@ def train_model(networkname = None, num_epochs = 10, batch_size = 200):
     # with np.load('model.npz') as f:
     #     param_values = [f['arr_%d' % i] for i in range(len(f.files))]
     # lasagne.layers.set_all_param_values(network, param_values)
+    print 'finished training!'
     losses['train_loss'] = training_loss_history
     losses['val_loss'] = validation_loss_history
 
